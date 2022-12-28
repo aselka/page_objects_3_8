cbffcf9d43eaf71ad0f0f1ae7ad6fb887a7c95fc
aselka <bis-aselya@yandex.ru>
2022-12-28 14:41:50 +0400

    change name locator

--- demoqa_tests/model/controls/checkbox.py
+++ demoqa_tests/model/controls/checkbox.py
@@ -1,6 +1,6 @@
 from selene import have
 
 
-def checkboxes_click(locator, *by_texts):
+def checkboxes_click(elements, *by_texts):
     for value in by_texts:
-        locator.element_by(have.text(value)).click()
+        elements.element_by(have.text(value)).click()
--- demoqa_tests/model/controls/radiobutton.py
+++ demoqa_tests/model/controls/radiobutton.py
@@ -1,5 +1,5 @@
 from selene import have
 
 
-def set_value(element, text):
-    element.element_by(have.value(text)).element('..').click()
+def set_value(elements, text):
+    elements.element_by(have.value(text)).element('..').click()
